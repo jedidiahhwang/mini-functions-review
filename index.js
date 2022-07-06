96a03fcfa02bf2461495167e920c75b5d1d284c0
Jedidiah <jedidiah.hwang.dev@gmail.com>
2022-07-06 12:47:47 -0600

    initial review

---
+++ index.js
@@ -0,0 +1,114 @@
+/*
+    FUNCTIONS REVIEW
+
+    The following assignment is used to brush up the fundamentals of functions.
+
+    For the problems listed below, please accomplish the following...
+        1) Write out comments for each line of code explaining what it's doing, for example...
+
+            function func() { // Creating a new function called "func".
+
+            }
+        2) Write the functionality of the function to the best of your ability.
+        3) Write the function IN THE SYNTAX YOU'RE LEAST COMFORTABLE WITH. If you hate arrow functions, answer with an arrow function, etc.
+
+    If a question is just asking for a short answer, simply write it within the comment block.
+
+    I'm not looking for whether you know how to answer the questions with code, I want you to dissect each line of code carefully
+    while showing your logic in solving the problem.
+*/
+
+console.log("Hello world");
+
+/*
+    PROBLEM 1: Why do we use functions?
+    
+    ANSWER: 
+*/
+
+
+
+/*
+    PROBLEM 2: What is a parameter? How does it differ from an argument?
+
+    ANSWER:
+*/
+
+
+
+/*
+    PROBLEM 3: For the following terms, write out the appropriate function syntax.
+
+    ANSWER:
+
+        FUNCTION DECLARATION
+            
+
+
+        FUNCTION EXPRESSION
+            
+
+
+        ARROW FUNCTION
+            
+
+
+*/
+
+
+
+/*
+    PROBLEM 4: When do we want to return something?
+
+    ANSWER:
+*/
+
+
+
+
+/*
+    PROBLEM 5: Create a function called introduction. This function will take no parameters and have no functionality in it.
+*/
+
+// CODE HERE
+
+
+
+/*
+    PROBLEM 6: Copy your answer from above and paste it here. Now, give this function function will one parameter called "name". 
+    This function will still have no functionality in it.
+*/
+
+// CODE HERE
+
+
+
+/*
+    PROBLEM 7: Copy your answer from above and paste it here. Write some functionality that will return a string of
+    "Hello, my name is [INSERT NAME HERE]".
+*/
+
+// CODE HERE
+
+
+
+/*
+    PROBLEM 8: Write a function that takes 2 parameters and returns the sum.
+*/
+
+// CODE HERE
+
+
+
+/*
+    PROBLEM 8: Write a function that takes in an array as a parameter. Print out every name in the array.
+*/
+
+// CODE HERE
+
+
+
+/*
+    PROBLEM 9: Write a function that takes in a number as a parameter. Check if the parameter is even. 
+    If it is, return "The number is even". If it is not, return "The number is odd".
+*/
